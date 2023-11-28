60b47b795d0f7e2d008ec24632b35164e4d13d47
keanna pye <keannapye5@gmail.com>
2023-11-28 15:29:45 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,27 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        var drawCircle = function() {
+            // Code to draw a circle
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
-        // TODO 3 / 7 : Call the drawCircle() function 
+               // TODO 3 / 7 : Call the drawCircle() function
+for(var i = 1; i <= 100; i++){
+    drawCircle();
+}
+// drawCircle();
+// drawCircle();
+// drawCircle();
+// drawCircle();
+// drawCircle();
 
 
         ////////////////////////////////////////////////////////////
@@ -39,16 +54,31 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            // physikz.updatePosition(circle[0]);
+            // physikz.updatePosition(circle[1]);
+            // physikz.updatePosition(circle[2]);
+            // physikz.updatePosition(circle[3]);
+            // physikz.updatePosition(circle[4]);
 
-            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            
+
+            // TODO 5 : Call game.checkCirclePosition() on your circles.
+// game.checkCirclePosition(circles[0]);
+// game.checkCirclePosition(circles[1]);
+// game.checkCirclePosition(circles[2]);
+// game.checkCirclePosition(circles[3]);
+// game.checkCirclePosition(circles[4]);
+
 
             // TODO 9 : Iterate over the array
-           
-            
+            for(var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+
         }
-    
+    }
+           
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -60,9 +90,19 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
+
+
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+                        if (circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if(circle.y < 0){
+                circle.y = canvas.height;
+            }
+            if(circle.y > canvas.height){
+                circle.y = 0;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,39 +28,37 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100) {
+      createPlatform(i, canvas.height, -1, -canvas.height);
+    }
+    for (let i = 100; i < canvas.height; i += 100) {
+      createPlatform(canvas.width, i, -canvas.width, -1);
+    }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
     /////////////////////////////////////////////////
 
     // TODO 1
-    // Create platforms
-    // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height)
-
-
-
+    createPlatform(1000, 650, 200, 25);
+    createPlatform(600, 540, 200, 25);
+    createPlatform(478, 479, 200, 25);
+    createPlatform(350, 419, 200, 25);
+    createPlatform(470,310 , 200, 25);
     
     // TODO 2
-    // Create collectables
-    // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
-    // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable('kennedi', 450,350, .4, 1)
+    createCollectable('database', 500, 250, .4, 1 )
+    createCollectable('grace', 1000, 500,.7, 1)
 
 
 
 
     // TODO 3
-    // Create cannons
-    // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
-    // Your wall choices are: 'top' 'left' 'right' and 'bottom'
-    // example usage: createCannon(side, position, delay, width, height)
+    createCannon('top', 200, 700, 25, 25 )
+    createCannon('right', 120, 700, 25, 25 )
+    createCannon('bottom', 900, 700, 25, 25)
+    
 
 
 
